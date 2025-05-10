7a5e05d0d2b0da36e536b208bd75cb04981a28a7
Lennier Mulder <lenniermulder@icloud.com>
2025-05-10 11:21:28 -0700

    initial attempt

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -1,22 +1,28 @@
 import java.util.*;
 
-public class WordList
-{
-  private ArrayList <String> myList;
+public class WordList {
+    private ArrayList <String> myList;
 
-  public WordList() { myList = new ArrayList<String>(); }
-  public void add(String word) { myList.add(word); }
-  public String toString() { return myList.toString(); }
+    public WordList() { myList = new ArrayList<String>(); }
+    public void add(String word) { myList.add(word); }
+    public String toString() { return myList.toString(); }
 
-  public int numWordsOfLength(int len)
-  {
-    /* to be implemented in part (a) */
-    
-  }
+    public int numWordsOfLength(int len) {
+        int count = 0;
+        for (String s : myList) {
+            if (s.length() == len) count++;
+        }
+        return count;
+    }
 
-  public void removeWordsOfLength(int len)
-  {
-    /* to be implemented in part (b) */
-
-  }
+    public void removeWordsOfLength(int len) {
+        int i = 0;
+        while (i <= myList.size()) {
+            if (myList.get(i).length() == len) {
+                myList.remove(i);
+            } else {
+                i++;
+            }
+        }
+    }
 }
